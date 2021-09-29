4703c6484b95fa7127f5165ef30c845393627042
k.nowojewski <k.nowojewski@ideo.pl>
2021-09-29 22:44:18 +0200

    Miniscroll card styling

--- src/components/partials/MiniscrollCard.vue
+++ src/components/partials/MiniscrollCard.vue
@@ -2,6 +2,24 @@
   <div class="ms-card">
     <div class="ms-card__side front">
       <MiniscrollTop :miniscroll-data="miniscrollData" />
+      <div class="ms-card__stats">
+        <div class="ms-card__stat">
+          <span class="ms-icons-arrow-up"></span>
+          <span class="ms-card__stat-value">{{ miniscrollData.move }}"</span>
+        </div>
+        <div class="ms-card__stat">
+          <span class="ms-icons-shield"></span>
+          <span class="ms-card__stat-value">{{ miniscrollData.save }}</span>
+        </div>
+        <div class="ms-card__stat">
+          <span class="ms-icons-skull"></span>
+          <span class="ms-card__stat-value">{{ miniscrollData.wounds }}</span>
+        </div>
+        <div class="ms-card__stat">
+          <span class="ms-icons-star"></span>
+          <span class="ms-card__stat-value">{{ miniscrollData.bravery }}</span>
+        </div>
+      </div>
     </div>
     <div v-if="cardBack" class="ms-card__side back">
       <MiniscrollTop :miniscroll-data="miniscrollData" />
@@ -32,14 +50,41 @@ export default class MiniscrollCard extends Vue {
   margin: 0 auto;
   border: 1px solid $black;
   overflow: hidden;
+  background-color: $white;
+
+  *:not(span[class*="ms-icons"]) {
+    font-family: sans-serif !important;
+  }
 
   &__side {
     width: 340px;
     height: 240px;
+    padding: 4px 8px;
 
     &.back {
       border-top: 1px solid $black;
     }
+  }
+
+  &__stats {
+    padding: 3px 0;
+    display: flex;
+    justify-content: space-around;
+    border-bottom: 1px solid $black;
+  }
+
+  &__stat {
+    display: flex;
+    align-items: center;
+
+    span[class*="ms-icons"] {
+      font-size: 25px;
+      margin-right: 4px;
+    }
+  }
+
+  &__stat-value {
+    font-size: 20px;
   }
 }
 </style>
--- src/components/partials/miniscroll-parts/MiniscrollTop.vue
+++ src/components/partials/miniscroll-parts/MiniscrollTop.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="ms-card__top">
-    <div class="ms-card__top-name">
+    <div class="ms-card__top-left">
       <p class="ms-card__top-title">{{ miniscrollData.name }}</p>
-      <p class="ms-card__top-mount">On {{ miniscrollData.mount }}</p>
+      <p v-if="miniscrollData.mount" class="ms-card__top-mount">
+        On {{ miniscrollData.mount }}
+      </p>
     </div>
-    <div class="ms-card__top-weapons">
-      <p>Weapon 1 / Weapon 2</p>
+    <div class="ms-card__top-right">
+      <p class="ms-card__top-weapons">Weapon 1 / Weapon 2</p>
     </div>
   </div>
 </template>
@@ -20,4 +22,40 @@ export default class MiniscrollTop extends Vue {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.ms-card {
+  &__top {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    border-bottom: 1px solid $black;
+  }
+
+  &__top-left {
+    width: 60%;
+    padding-right: 16px;
+    text-transform: uppercase;
+  }
+
+  &__top-right {
+    width: 40%;
+  }
+
+  &__top-title {
+    font-size: 12px;
+    font-weight: 700;
+    line-height: unset;
+  }
+
+  &__top-mount {
+    font-size: 10px;
+    line-height: unset;
+  }
+
+  &__top-weapons {
+    text-align: right;
+    font-size: 9px;
+    line-height: unset;
+  }
+}
+</style>
